6c254c3a84737a71b7fc85a106e36fd3f690711e
Tod Fleeman <rtoddf@me.com>
2019-06-12 21:57:03 -0400

    got through react intro #17

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const element = React.createElement('p', null, 'This is the heading');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// const element = React.createElement('ol', null,
+//     tasks.map((task, index) => React.createElement('li', {key: index}, task))
+// );
+
+const tasks = ['bob', 'mark', 'steve'];
+
+const element = (
+    <div>
+        <h1>Task List</h1>
+        <ol>
+            {tasks.map((task, index) => <li key={index}>{task}</li>)}
+        </ol>
+    </div>);
+
+ReactDom.render(element, document.getElementById('root'));
